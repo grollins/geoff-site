e8f262c7e0eba38046a93df52ed69267abf8c34c
Geoff Rollins <grollins@gmail.com>
2019-12-30 10:59:10 -0600

    Move favicon to assets

--- deploy_to_gae.sh
+++ deploy_to_gae.sh
@@ -2,11 +2,11 @@
 
 rm -rf static
 mkdir static
-cp favicon.ico ./static/
 cp index.html ./static/
 mkdir static/assets
 cp -r assets/css static/assets/
 cp -r assets/images static/assets/
+cp assets/favicon.ico ./static/assets/
 rm -rf ~/GAE/geoff-site/static
 cp -r static ~/GAE/geoff-site/static
 cp app.yaml ~/GAE/geoff-site/
